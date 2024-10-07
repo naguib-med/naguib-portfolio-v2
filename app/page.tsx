00a74c40934b545fea3b229a21d29193907c863c
Naguib <moma.naguib@gmail.com>
2024-10-07 14:56:48 +0200

    added Status component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./_components/Header";
 import { Hero } from "./_components/Hero";
 import { Spacing } from "./_components/Spacing";
+import {Status} from "@/app/_components/Status";
 
 export default function Home() {
   return (
@@ -10,6 +11,9 @@ export default function Home() {
       <Spacing size="md" />
       <Hero />
       <Spacing size="md" />
+      <Status />
+
+        <Spacing size="md"/>    
     </main>
   );
 }
