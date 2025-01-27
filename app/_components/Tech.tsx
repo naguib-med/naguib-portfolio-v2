679f91e61c94d0f6770ae47f33235ce6d14629af
Naguib <moma.naguib@gmail.com>
2025-01-27 22:45:30 +0100

    style: standardize import statements in Tech component

--- app/_components/Tech.tsx
+++ app/_components/Tech.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import { Section } from "./Section";
 import { TailwindIcon } from "./icons/TailwindIcon";
 import { ReactIcon } from "./icons/ReactIcon";
