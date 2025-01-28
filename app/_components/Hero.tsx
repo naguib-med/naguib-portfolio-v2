7fa00c6087639b8efd858fc624e5317bab6ac55b
Naguib <moma.naguib@gmail.com>
2025-01-28 13:54:53 +0100

    fix: update CV file name and link in Hero component

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -131,7 +131,7 @@ export default function Hero() {
                     className="mt-2"
                 >
                     <motion.a
-                        href="/CV_Naguib.pdf"
+                        href="/cv_ingenieur_fullstack_naguib.pdf"
                         download
                         whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }}
